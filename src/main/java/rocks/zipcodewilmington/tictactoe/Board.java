425acf3dcbddc001a5075a9315549b04bbc43b3e
nicholasmcnamra <nikomcnamara@gmail.com>
2024-03-10 21:21:54 -0400

    finished

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,58 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    private Character[][] matrix;
     public Board(Character[][] matrix) {
+        this.matrix = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        return checkWinner('X', matrix);
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        return checkWinner('O', matrix);
+    }
+
+    private boolean checkWinner(char symbol, Character [][] matrix) {
+        int row = matrix.length;
+        int column = matrix[0].length;
+
+        boolean winner = false;
+        for (int i = 0; i < row; i++) {
+                if (matrix[0][i].equals(symbol) && matrix[1][i].equals(symbol) && matrix[2][i].equals(symbol)) {
+                    winner = true;
+                }
+                else if (matrix[i][0].equals(symbol) && matrix[i][1] == symbol && matrix[i][2] == symbol) {
+                    winner = true;
+                }
+                else if (matrix[0][0].equals(symbol) && matrix[1][1].equals(symbol) && matrix[2][2].equals(symbol)) {
+                    winner = true;
+                }
+                else if (matrix[2][0].equals(symbol) && matrix[1][1].equals(symbol) && matrix[0][2].equals(symbol)) {
+                    winner = true;
+                }
+        }
+        return winner;
     }
 
     public Boolean isTie() {
-        return null;
+        boolean tie = false;
+        if (getWinner() == "") {
+            tie = true;
+        }
+        return tie;
     }
 
     public String getWinner() {
-        return null;
+        if(isInFavorOfX()) {
+            return "X";
+        }
+        else if (isInFavorOfO()) {
+            return "O";
+        }
+        else {
+            return "";
+        }
     }
-
 }
